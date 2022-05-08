3c7ccc6a088d8d77ed777653a9a216aa220c64f7
Hmohammed2 <hamza_mohammed15@hotmail.com>
2022-05-08 18:39:04 +0100

    Updated pagination functionality onto methods

--- scraper.py
+++ scraper.py
@@ -8,20 +8,21 @@ from typing import List
 @dataclass
 class Data:
     Title: List[str] = field(default_factory=list)
+    Name: List[str] = field(default_factory=list)
+    Hours_played: List[str] = field(default_factory=list)
 
 
 class Scraper:
 
-    url = "https://steamplayercount.com/popular"
+    url = "https://steamcharts.com/top"
 
-    def __init__(self, response=None, get_url=url, page_number: int = 1):
+    def __init__(self, response=None, get_url=url):
         self.response = self.get_status if response is None else response
         self.get_url = get_url
-        self.page_number = page_number
 
-    def get_status(self):
+    def get_status(self, page_number=None):
         try:
-            r = requests.get(self.pagination(self.page_number))
+            r = requests.get(self.get_url) if page_number is None else requests.get(self.pagination(page_number))
             return r
         except requests.exceptions.Timeout:
             print("Session Timeout")
@@ -33,9 +34,9 @@ class Scraper:
             # catastrophic error. bail.
             raise SystemExit(e)
 
-    def return_soup(self):
+    def return_soup(self, page_number=None):
         # return soup object
-        request = self.get_status()
+        request = self.get_status() if page_number is None else self.get_status(page_number)
         soup = BeautifulSoup(request.text, "html.parser")
         return soup
 
@@ -45,19 +46,18 @@ class Scraper:
         return soup.prettify()
 
     def pagination(self, page_num:int):
-        return f"{self.get_url}?page={page_num}"
+        return f"{self.get_url}/p.{page_num}"
 
-    def extract_into_list(self, tag=str, class_str=None, index=None, soup=None):
+    def extract_into_list(self, tag=str, class_str=None, href=False,index=None, soup=None):
         empty_list = []
 
         soup = self.return_soup() if soup is None else soup
 
-        all_tags = soup.find_all(tag, class_=class_str)
+        all_tags = soup.find_all(tag, class_=class_str, href=href)
 
         if not isinstance(soup, BeautifulSoup):
             raise TypeError
         else:
-
             if index is None:
                 for container in all_tags:
                     name = container.text
@@ -76,11 +76,9 @@ if __name__ == "__main__":
     while True:
         try:
             page_counter += 1
-            print(scraper.extract_into_list("a", "app-link"))
+            data = scraper.return_soup(page_counter)
+            print(scraper.extract_into_list(tag="td", class_str="num", soup=data))
         except Exception as ex:
             print(ex)
             print("probably last page:", page_counter)
             break
-
-
-
