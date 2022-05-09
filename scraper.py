8df21b26d26ad0a254d94a3d81fb7ddbe995ac7e
Hmohammed2 <hamza_mohammed15@hotmail.com>
2022-05-09 16:58:06 +0100

    Added function to convert dictionary into dataframe. This could then be processed into an excel file

--- scraper.py
+++ scraper.py
@@ -1,20 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from dataclasses import dataclass, field
-from typing import List
 import re
+import openpyxl
 
 
-@dataclass
 class Data:
-    Product_name: List[str] = field(default_factory=list)
-    Price: List[str] = field(default_factory=list)
-    Summary: List[str] = field(default_factory=list)
+    def __init__(self, product_name=None, price=None, summary=None, *args, **kwargs):
+        self.product_name = product_name
+        self.price = price
+        self.summary = summary
+
+    def __repr__(self):
+        return f"Product={self.product_name}, Price={self.price}, summary={self.summary}"
+
+    def to_dict(self):
+        return {"Product": self.product_name, "Price": self.price, "Summary": self.summary}
 
 
 class Scraper:
-
     default_url = "https://www.arcadeworlduk.com/"
 
     def __init__(self, response=None, get_url=default_url, **kwargs):
@@ -24,7 +28,8 @@ class Scraper:
 
     def get_status(self, page_number=None):
         try:
-            r = requests.get(self.search_product()) if page_number is None else requests.get(self.pagination(page_number))
+            r = requests.get(self.search_product()) if page_number is None else requests.get(
+                self.pagination(page_number))
             return r
         except requests.exceptions.Timeout:
             print("Session Timeout")
@@ -77,12 +82,7 @@ class Scraper:
         return empty_list
 
 
-class ToCsv:
-    pass
-
-
 def main(iterate=False):
-
     page_counter = 1
     scraper = Scraper()
     if iterate is True:
@@ -90,9 +90,13 @@ def main(iterate=False):
             try:
                 page_counter += 1
                 data = scraper.return_soup(page_counter)
-                print(scraper.extract_into_list(tag="a", href=True, soup=data, attrs={"data-event-type": True}))
-                print(scraper.extract_into_list(tag="span", class_str="price", soup=data))
-                print(scraper.extract_into_list(tag="p", class_str="card-summary", soup=data))
+                product_name = scraper.extract_into_list(tag="a", href=True, soup=data, attrs={"data-event-type": True})
+                price = scraper.extract_into_list(tag="span", class_str="price", soup=data,
+                                                  attrs={"data-product-price-with"
+                                                         "-tax": True})
+                summary = scraper.extract_into_list(tag="p", class_str="card-summary", soup=data)
+                data_fields = Data(product_name, price, summary)
+                print(data_fields)
             except Exception as ex:
                 print(ex)
                 print("probably last page:", page_counter)
@@ -100,13 +104,17 @@ def main(iterate=False):
     else:
         try:
             data = scraper.return_soup(page_counter)
-            print(scraper.extract_into_list(tag="a", href=True, soup=data, attrs={"data-event-type": True}))
-            print(scraper.extract_into_list(tag="span", class_str="price", soup=data, attrs={"data-product-price-with"
-                                                                                             "-tax": True}))
-            print(scraper.extract_into_list(tag="p", class_str="card-summary", soup=data))
+            product_name = scraper.extract_into_list(tag="a", href=True, soup=data, attrs={"data-event-type": True})
+            price = scraper.extract_into_list(tag="span", class_str="price", soup=data, attrs={"data-product-price-with"
+                                                                                               "-tax": True})
+            summary = scraper.extract_into_list(tag="p", class_str="card-summary", soup=data)
+            data_fields = Data(product_name, price, summary)
+            df = pd.DataFrame(data_fields.to_dict())
+            df.to_excel("Output.xlsx")
+            print(df)
         except Exception as ex:
             print(ex)
 
 
 if __name__ == "__main__":
-    main(True)
+    main()
